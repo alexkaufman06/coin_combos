c580ee5d0ec02ee7a59f35c12298ee38d6a670cd
Alex Kaufman <alexkaufman06@gmail.com>
2015-02-11 11:02:25 -0800

    Fix function and specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,16 @@
 var coinCombo = function(change) {
-  var quarters = Math.floor(change / 25);
-  var quartersChange = change % 25;
-  var dimesChange = quartersChange % 10;
-  var dimes = Math.floor(quartersChange / 10);
-  var nicklesChange = dimesChange % 5;
-  var nickles = Math.floor(dimesChange / 5);
-  var penniesChange = nicklesChange % 1;
-  var pennies = Math.floor(nicklesChange / 1);
-
-  if (quartersChange === 0) {
-    return quarters + "Q";
-  } else if (dimesChange === 0) {
-    return quarters + "Q " + dimes + "D";
-  } else {
-    return quarters + "Q " + dimes + "D " + nickles + "N";
-  }
+  var answer = [0,0,0,0]
+  while (change >= 25) {
+    answer[0] += 1;
+    change -= 25;
+  } while (change >= 10) {
+    answer[1] += 1;
+    change -= 10;
+  } while (change >= 5) {
+    answer[2] += 1;
+    change -= 5;
+  } while (change >= 1) {
+    answer[3] += 1;
+    change -= 1;
+  } return answer;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,13 +1,17 @@
 describe("coinCombo", function() {
   it("returns the correct amount of quarters", function() {
-    expect(coinCombo(50)).to.equal("2Q");
+    expect(coinCombo(50)).to.eql([ 2, 0, 0, 0 ]);
   });
 
   it("returns the correct amount of dimes", function() {
-    expect(coinCombo(35)).to.equal("1Q 1D");
+    expect(coinCombo(35)).to.eql([ 1, 1, 0, 0 ]);
   });
 
   it("returns the correct amount of nickles", function() {
-    expect(coinCombo(40)).to.equal("1Q 1D 1N");
+    expect(coinCombo(40)).to.eql([ 1, 1, 1, 0 ]);
+  });
+
+  it("returns the correct amount of pennies", function() {
+    expect(coinCombo(99)).to.eql([ 3, 2, 0, 4 ]);
   });
 });
