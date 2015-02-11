475f735f15d0f3580198a3379e86fafbfa037951
Philip <Philiporchard@gmail.com>
2015-02-11 09:55:36 -0800

    Add quarters function

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var coinCombo = function(change) {
+  var quarters = change / 25;
+  return quarters + "Q";
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,6 @@
 describe("coinCombo", function() {
   it("returns the correct amount of quarters", function() {
-    expect(coinCombo(50)).to.equal.("2Q");
+    expect(coinCombo(50)).to.equal("2Q");
   });
 
 });
