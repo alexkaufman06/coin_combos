de367a162b1f74fd27c2fa35deff2387c769250f
Philip <Philiporchard@gmail.com>
2015-02-11 10:14:21 -0800

    Add nickles spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 var coinCombo = function(change) {
-  var quarters = change / 25;
-  return quarters + "Q";
+  var quarters = Math.floor(change / 25);
+  var dimes = (change % 25) / 10;
+
+  if(change % 25 === 0) {
+    return quarters + "Q";
+  } else {
+    return quarters + "Q " + dimes + "D";
+  }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -3,4 +3,11 @@ describe("coinCombo", function() {
     expect(coinCombo(50)).to.equal("2Q");
   });
 
+  it("returns the correct amount of dimes", function() {
+    expect(coinCombo(35)).to.equal("1Q 1D");
+  });
+
+  it("returns the correct amount of nickles", function() {
+    expect(coinCombos(40)).to.equal("1Q 1D 1N");
+  });
 });
