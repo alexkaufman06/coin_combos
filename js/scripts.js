d85b631bd0b06836125de7ea3b47673b97ef6a91
phil <PhilipOrchard@gmail.com>
2015-02-11 16:09:02 -0800

    Optimized function

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,12 @@
 var coinCombo = function(change) {
   var answer = [0,0,0,0]
-  while (change >= 25) {
-    answer[0] += 1;
-    change -= 25;
-  } while (change >= 10) {
-    answer[1] += 1;
-    change -= 10;
-  } while (change >= 5) {
-    answer[2] += 1;
-    change -= 5;
-  } while (change >= 1) {
-    answer[3] += 1;
-    change -= 1;
+  var coins = [25, 10, 5, 1]
+
+  for (var i=0; i < coins.length; i++) {
+    while (change >= coins[i]) {
+    answer[i] += 1;
+    change -= coins[i];
+    }
   } return answer;
 };
 
